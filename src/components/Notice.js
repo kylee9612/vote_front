0ddde41919bd8435a12ea5024ace4336da945133
sank2168 <sank2168@axiasoft.com>
2023-03-29 18:05:58 +0900

    [L]noticeList 분할 작업_3

--- src/components/Notice.js
+++ src/components/Notice.js
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 /** CSS **/
 import './css/NoticeList.css';
 /** Component **/
 import Modal from "./Util/Modal";
+import axios from "axios";
 
-function Notice({notice}) {
+function Notice({searchText, searchType, page}) {
+    let noticeList = "";
+    let returnMap = "";
     /**open Detail**/
     const [modalVisibleId, setModalVisibleId] = useState("")
     const onModalHandler = id => {
@@ -15,25 +18,97 @@ function Notice({notice}) {
     }
     /**open Detail**/
 
-    return (notice && notice.map((notice, index) => (
-                <tr key={index} className={"detail_" + notice.num}>
-                    {/*<input type={"hidden"} id="detail" value={JSON.stringify(notice)} />*/}
-                    <td onClick={() => onModalHandler(notice.num)}>{notice.num}</td>
-                    <td onClick={() => onModalHandler(notice.num)}>{notice.title}</td>
-                    <td onClick={() => onModalHandler(notice.num)} style={{textAlign: 'center'}}>{notice.count}</td>
-                    <td onClick={() => onModalHandler(notice.num)}>{notice.date}</td>
+    useEffect(() => {
+        axios.get('/api/notice/getNoticeList', {
+            params: {
+                "searchText": searchText,
+                "searchType": searchType,
+                "page": page
+            }
+        }).then((response) => {
 
-                    <Modal id={notice.num}
-                           modalVisibleId={modalVisibleId}
-                           setModalVisibleId={onCloseHandler}
+            console.log(
+                "asd ::" +
+                Array.from(response.data).map((notice,index)=>{
+                    console.log("notice :: " +notice)
+                    console.log("notice.nt_contents :: " + notice.nt_contents)
+                }))
+            noticeList = response.data;
 
-                           header={notice.title}
-                           contents={notice.contents}
-                           date={notice.date}
-                           count={notice.count}
-                    />
-                </tr>
-    )))
+            returnMap = noticeList.map((notice, index) => (
+                    <tr key={index} className={"detail_" + notice.nt_no}>
+                        <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_no}</td>
+                        <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_title}</td>
+                        <td onClick={() => onModalHandler(notice.nt_no)} style={{textAlign: 'center'}}>{notice.nt_count}</td>
+                        <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_contents}</td>
+
+                        <Modal id={notice.nt_no}
+                               modalVisibleId={modalVisibleId}
+                               setModalVisibleId={onCloseHandler}
+
+                               header={notice.nt_title}
+                               contents={notice.nt_contents}
+                               date={notice.reg_date}
+                               count={notice.nt_count}
+                        />
+                    </tr>
+                ))
+            console.log("returnMap  " + returnMap);
+        }).catch((error) => {
+            alert('error' + error)
+            noticeList = null;
+            // return (alert('error' + error))
+            // return (<p>no exists Notice</p>)
+        })
+    });
+    // 1. FAILED
+    // return (
+    //         noticeList === null ?
+    //         <p>no exists Notice</p>
+    //         : returnMap
+    // )
+    // 2. FAILED
+    // return (Array.from(noticeList).map((notice, index) => (
+    //     <tr key={index} className={"detail_" + notice.nt_no}>
+    //         <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_no}</td>
+    //         <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_title}</td>
+    //         <td onClick={() => onModalHandler(notice.nt_no)} style={{textAlign: 'center'}}>{notice.nt_count}</td>
+    //         <td onClick={() => onModalHandler(notice.nt_no)}>{notice.nt_contents}</td>
+    //
+    //         <Modal id={notice.nt_no}
+    //                modalVisibleId={modalVisibleId}
+    //                setModalVisibleId={onCloseHandler}
+    //
+    //                header={notice.nt_title}
+    //                contents={notice.nt_contents}
+    //                date={notice.reg_date}
+    //                count={notice.nt_count}
+    //         />
+    //     </tr>
+    // )))
+    // 3. FAILED
+    //     return (
+    //         for(let i = 0 ; i < noticeList.length ; i ++){
+    //                 <tr className={"detail_" + noticeList(i).nt_no}>
+    //                      <td onClick={() => onModalHandler(noticeList(i).nt_no)}>{noticeList(i).nt_no}</td>
+    //                      <td onClick={() => onModalHandler(noticeList(i).nt_no)}>{noticeList(i).nt_title}</td>
+    //                      <td onClick={() => onModalHandler(noticeList(i).nt_no)} style={{textAlign: 'center'}}>{noticeList(i).nt_count}</td>
+    //                      <td onClick={() => onModalHandler(noticeList(i).nt_no)}>{noticeList(i).nt_contents}</td>
+    //
+    //                      <Modal id={noticeList(i).nt_no}
+    //                             modalVisibleId={modalVisibleId}
+    //                             setModalVisibleId={onCloseHandler}
+    //
+    //                            header={noticeList(i).nt_title}
+    //                            contents={noticeList(i).nt_contents}
+    //                            date={noticeList(i).reg_date}
+    //                             count={noticeList(i).nt_count}
+    //                     />
+    //                  </tr>
+    //     }
+    //     )
+
+
 }
 
 export default Notice;
